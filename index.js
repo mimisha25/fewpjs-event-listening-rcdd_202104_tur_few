fb8a449d984b35445a590d0d4a170c2c2610fd4a
Aziza  iliasova <miacorporation25@gmail.com>
2021-04-27 04:50:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function addEventListener(){
+function addingEventListener(){
 const input = document.getElementById('input');
 input.addEventListener('click', function(event) {
   alert('I was clicked!');
